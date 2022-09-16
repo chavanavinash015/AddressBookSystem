df691ee82b94beb2917721dca77c5a9db96a2707
arc-015 <chavanavinash015@gmail.com>
2022-09-16 16:56:42 +0530

    "Create Contact person ."

--- Day9AddressBook/src/AddressBook.java
+++ Day9AddressBook/src/AddressBook.java
@@ -4,5 +4,27 @@ public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book ");
 
+		class person {
+			private int Id;
+			private String fullName;
+			private String email;
+			private String city;
+			private String state;
+			private String zipcode;
+			private String phonenumber;
+
+			person(int Id, String fullName, String email, String city, String state, String zipcode,
+					String phonenumber) {
+
+				this.Id = Id;
+				this.fullName = fullName;
+				this.email = email;
+				this.city = city;
+				this.state = state;
+				this.zipcode = zipcode;
+				this.phonenumber = phonenumber;
+			}
+
+		}
 	}
 }
